64507cc6c33c5d7770039946c7253eb32e6c6fc3
Tarek Hammami <44512777+TKHMx7@users.noreply.github.com>
2019-01-23 00:25:52 +0100

    Add files via upload

---
+++ myJS.js
@@ -0,0 +1,8 @@
+function myfunction() {
+
+  str = document.f.mySearch.value;
+  if (str=="") { alert("Type something to search.."); } else {
+  alert("You searched for \"" + str + "\". Soon Inshallah you'll be able to.");
+  }
+
+}
